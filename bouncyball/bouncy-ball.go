6f438c84649ed31011c428cbc2d0d988c5906dba
R. Aidan Campbell <aidan.campbell@gmail.com>
2022-03-05 10:28:11 -0700

    multiple balls, no physics between balls

--- bouncyball/bouncy-ball.go
+++ bouncyball/bouncy-ball.go
@@ -7,44 +7,48 @@ import (
 	"github.com/raidancampbell/go-gfx/internal"
 	"github.com/raidancampbell/go-gfx/perlin"
 	"github.com/raidancampbell/libraidan/pkg/roper"
+	"math/rand"
 )
 
-type state struct {
+type ballState struct {
 	bLocation pixel.Vec
 	bVelocity pixel.Vec
 }
 
-var s state
+var s []ballState
 
 func applyGravity() {
-	s.bVelocity = s.bVelocity.Add(pixel.Vec{X: 0., Y:-0.1})
+	for i := range s {
+		s[i].bVelocity = s[i].bVelocity.Add(pixel.Vec{X: 0., Y:-0.1})
+	}
 }
 
 func getBallRadius() float64 {
-	return 0.05 * float64(internal.WINDOW_HEIGHT)
+	return 0.03 * float64(internal.WINDOW_HEIGHT)
 }
 
-func drawBall(imd *imdraw.IMDraw) {
-	imd.Push(s.bLocation)
-
+func drawBalls(imd *imdraw.IMDraw) {
+	for i := range s {
+		imd.Push(s[i].bLocation)
+	}
 	imd.Circle(getBallRadius(), 0)
 }
 
-func checkCollision(ground []pixel.Vec) bool {
+func checkCollision(ground []pixel.Vec, i int) bool {
 	// collide with left/right walls
-	if s.bLocation.X - getBallRadius() < 0 {
-		s.bVelocity.X *= -1
-		s.bLocation.X = 0 + getBallRadius()
+	if s[i].bLocation.X - getBallRadius() < 0 {
+		s[i].bVelocity.X *= -1
+		s[i].bLocation.X = 0 + getBallRadius()
 		return true
-	} else if s.bLocation.X + getBallRadius() > float64(internal.WINDOW_WIDTH) {
-		s.bVelocity.X *= -1
-		s.bLocation.X = float64(internal.WINDOW_WIDTH) - getBallRadius()
+	} else if s[i].bLocation.X + getBallRadius() > float64(internal.WINDOW_WIDTH) {
+		s[i].bVelocity.X *= -1
+		s[i].bLocation.X = float64(internal.WINDOW_WIDTH) - getBallRadius()
 		return true
 	}
 	// ceil
-	if s.bLocation.Y + getBallRadius() > float64(internal.WINDOW_HEIGHT) {
-		s.bVelocity.Y *= -1
-		s.bLocation.Y = float64(internal.WINDOW_HEIGHT) - getBallRadius()
+	if s[i].bLocation.Y + getBallRadius() > float64(internal.WINDOW_HEIGHT) {
+		s[i].bVelocity.Y *= -1
+		s[i].bLocation.Y = float64(internal.WINDOW_HEIGHT) - getBallRadius()
 		return true
 	}
 	// irregular ground
@@ -52,32 +56,32 @@ func checkCollision(ground []pixel.Vec) bool {
 	// 2-pass collision detection. first pass to find how much of the arc intersects
 	// second pass to calculate collision from middle of arc
 	var xIntersect []float64
-	for x := s.bLocation.X - getBallRadius(); x <= s.bLocation.X + getBallRadius(); x++ {
-		circ := pixel.C(s.bLocation, getBallRadius())
+	for x := s[i].bLocation.X - getBallRadius(); x <= s[i].bLocation.X + getBallRadius(); x++ {
+		circ := pixel.C(s[i].bLocation, getBallRadius())
 		if circ.Contains(ground[int(x)%len(ground)]) {
 			xIntersect = append(xIntersect, x)
 		}
 	}
 	if xIntersect != nil {
 		x := xIntersect[len(xIntersect)/2]
-		circ := pixel.C(s.bLocation, getBallRadius())
+		circ := pixel.C(s[i].bLocation, getBallRadius())
 		if circ.Contains(ground[int(x)%len(ground)]) {
 			// the velocity is reversed in the direction of the angle of impact
 			angle := circ.Center.To(ground[int(x)])
 			angle = angle.Scaled(-1.)
-			s.bVelocity = angle.Unit().Scaled(s.bVelocity.Len())
+			s[i].bVelocity = angle.Unit().Scaled(s[i].bVelocity.Len())
 
 			// back the circle out until it no longer intersects
-			for ;circ.Contains(ground[int(x) % len(ground)]);circ = circ.Moved(s.bVelocity.Unit()) {}
-			s.bLocation = circ.Center
+			for ;circ.Contains(ground[int(x) % len(ground)]);circ = circ.Moved(s[i].bVelocity.Unit()) {}
+			s[i].bLocation = circ.Center
 			return true
 		}
 	}
 
 	// placeholder to check math
-	if s.bLocation.Y - getBallRadius() < 0. {
-		s.bVelocity.Y *= -1
-		s.bLocation.Y = 0 + getBallRadius()
+	if s[i].bLocation.Y - getBallRadius() < 0. {
+		s[i].bVelocity.Y *= -1
+		s[i].bLocation.Y = 0 + getBallRadius()
 		return true
 	}
 	return false
@@ -85,19 +89,25 @@ func checkCollision(ground []pixel.Vec) bool {
 
 func Do(cfg *pixelgl.WindowConfig, imd *imdraw.IMDraw, offset float64) {
 	// always ensure the ball is drawn,
-	defer drawBall(imd)
+	defer drawBalls(imd)
 
 	//draw an interesting surface
 	points := perlin.Perlin2DTemporal(cfg, imd, offset)
 
-	// initialize state
+	// initialize ballState
 	if roper.IsDefaultValue(s) {
-		s.bLocation = pixel.Vec{X: float64(internal.WINDOW_WIDTH)/2, Y:float64(internal.WINDOW_HEIGHT) - getBallRadius()}
+		s = make([]ballState, 30)
+		for i := range s {
+			s[i] = ballState{}
+			s[i].bLocation = pixel.Vec{X: float64(internal.WINDOW_WIDTH)/2, Y:float64(internal.WINDOW_HEIGHT) - getBallRadius()}
+			s[i].bVelocity.X = rand.Float64() * 5. - 5.
+		}
 		applyGravity()
-		s.bVelocity.X = 10
 		return
 	}
 	applyGravity()
-	checkCollision(points)
-	s.bLocation = s.bLocation.Add(s.bVelocity)
+	for i := range s {
+		checkCollision(points, i)
+		s[i].bLocation = s[i].bLocation.Add(s[i].bVelocity)
+	}
 }
